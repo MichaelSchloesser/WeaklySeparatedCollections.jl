25be5c9c1c013c90c3da7918438024b13c8eac87
MichaelSchloesser <heinz.michael.schloesser@rwth-aachen.de>
2023-11-24 14:16:45 +0100

    uncommented deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -15,4 +15,4 @@ makedocs(;
     warnonly = true,
 )
 
-# deploydocs(; repo = "github.com/MichaelSchloesser/WeaklySeparatedCollections.jl", push_preview = true)
+deploydocs(; repo = "github.com/MichaelSchloesser/WeaklySeparatedCollections.jl", push_preview = true)
