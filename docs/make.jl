b746e920d9150354e785f60c4957ffbc54d6d44a
MichaelSchloesser <heinz.michael.schloesser@rwth-aachen.de>
2023-11-26 19:16:06 +0100

    try to get dummy-icon working

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,7 @@ makedocs(;
     sitename = "WeaklySeparatedCollections.jl",
     format = Documenter.HTML(;
         repolink = "https://github.com/MichaelSchloesser/WeaklySeparatedCollections.jl",
+        assets = ["assets/favicon.ico"],
     ),
     authors = "Michael Schlößer",
     warnonly = true,
